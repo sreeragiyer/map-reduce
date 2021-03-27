0bc8466fa2f11c24c098ac6f0cb04ed9c37d3443
Mahidhar Kommineni <mahidhar@Mahidhars-MacBook-Air.local>
2021-03-27 19:59:22 -0400

    Adding Map reduce specification file

---
+++ src/MapReduceSpecification.java
@@ -0,0 +1,17 @@
+public class MapReduceSpecification {
+    /**
+     * This is the constant 'N' which will indicate the number of mapper
+     * and reducer processes to be launched.
+     */
+    int numProcesses;
+    /**
+     * Takes the input file location where the input data for map reduce
+     * is present.
+     */
+    String inputFileLocation;
+    /**
+     * Takes the output file location where the output is writtern after the
+     * map reduce execution is completed.
+     */
+    String outputFileLocation;
+}
