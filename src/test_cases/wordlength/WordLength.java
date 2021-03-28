f248da8ce822f99ef77f939cb627b3c1b8c7a0ad
sreeragiyer <sreeragriyer@gmail.com>
2021-03-28 00:56:22 -0400

    wordlength changes

--- src/test_cases/wordlength/WordLength.java
+++ src/test_cases/wordlength/WordLength.java
@@ -11,8 +11,8 @@ public class WordLength {
         System.out.println("Hello world");
         MapReduceSpecification mrs = new MapReduceSpecification();
         mrs.numProcesses = 1;
-        mrs.inputFileLocation = "/Users/mahidhar/Documents/532/p1_mapreduce-team-18/out/production/MapReduce/data/loremipsum.txt";
-        mrs.outputFileLocation = "/Users/mahidhar/Documents/532/p1_mapreduce-team-18/src/test_cases_output/WordLength.txt";
+        mrs.inputFileLocation = "src/data/loremipsum.txt";
+        mrs.outputFileLocation = "/src/test_cases_output/WordLength.txt";
         mrs.mapper = new WordLengthMapper();
         mrs.reducer = new WordLengthReducer();
         MapReduce obj = new MapReduce();
