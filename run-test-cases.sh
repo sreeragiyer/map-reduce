3af2c56a727690c553a21f7cc14ce38518462eda
sreeragiyer <sreeragriyer@gmail.com>
2021-03-28 10:57:49 -0400

    test script

--- run-test-cases.sh
+++ run-test-cases.sh
@@ -1,4 +1,7 @@
 
-javac src/**/*.java
+#javac src/**/*.java
+find -name "*.java" > sources.txt
+javac @sources.txt
 java -cp ./src test_cases/wordlength/WordLength
 java -cp ./src test_cases/capitalize/Capitalize
+java -cp ./src test_cases/wordcount/WordCount
