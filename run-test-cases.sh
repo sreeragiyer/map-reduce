dd083e63d7bd1e7d2df339054475887c910f0cf3
sreeragiyer <sreeragriyer@gmail.com>
2021-03-28 11:55:05 -0400

    updated script file

--- run-test-cases.sh
+++ run-test-cases.sh
@@ -1,7 +1,11 @@
 
 #javac src/**/*.java
-find -name "*.java" > sources.txt
-javac @sources.txt
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  find -name "*.java" > sources.txt
+  javac @sources.txt
+else
+  javac src/**/*.java
+fi
 java -cp ./src test_cases/wordlength/WordLength
 java -cp ./src test_cases/capitalize/Capitalize
 java -cp ./src test_cases/wordcount/WordCount
