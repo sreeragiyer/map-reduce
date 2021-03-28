daba69a4afffa89d1df417daddd24351cfd1cb24
sreeragiyer <sreeragriyer@gmail.com>
2021-03-28 01:22:38 -0400

    script to run test cases

---
+++ run-test-cases.sh
@@ -0,0 +1,4 @@
+javac src/test_cases/wordlength/WordLength.java
+java src/test_cases/wordlength/WordLength
+javac src/test_cases/capitalize/Capitalize.java
+java src/test_cases/capitalize/Capitalize
